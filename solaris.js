60e0fc2718028c93241d4dd085ed6ec8e8136a48
Gerhard Dickescheid <gerhard@dickescheid.de>
2019-01-05 13:38:53 +0100

    update outdated dependencies, code adjustments

--- solaris.js
+++ solaris.js
@@ -1,37 +1,37 @@
-var fs = require('fs');
-var format = require('util').format;
-var serialport = require("serialport");
-var mqtt = require('mqtt');
+const fs = require('fs');
+const format = require('util').format;
+const SerialPort = require("serialport");
+const Readline = require('@serialport/parser-readline');
+const mqtt = require('mqtt');
 
-var mqttClient  = mqtt.connect('mqtt://tux.home');
-var SerialPort = serialport.SerialPort;
+/* with keepalive=0, serial receiving is completely blocked, with the standard value of keepalive=60,
+ * the serial receiving is blocked for 60s and then 2 messages are received consecutively
+ * workaround: set keepalive to 15 (which probably blocks for 15s)
+ */
+const mqttClient  = mqtt.connect('mqtt://tux.home', {keepalive: 15});
 
-var baseFileName = '/home/pi/solaris';
-var mqttConnected = false;
+const baseFileName = '/home/pi/solaris';
+let mqttConnected = false;
+const mqttTopic = "sensors/solaris"
 
-var lineMapping = ['?', '?', 'P1', 'P2', 'TK', 'TR', 'TS', 'TV', 'V', 'Status', 'P'];  
+const lineMapping = ['?', '?', 'P1', 'P2', 'TK', 'TR', 'TS', 'TV', 'V', 'Status', 'P'];  
 
-var serial = new SerialPort("/dev/ttyAMA0", {
-  baudrate: 19200,
-  parser: serialport.parsers.readline("\r")
+const port = new SerialPort("/dev/ttyAMA0", {
+  baudRate: 19200,
 });
 
-var mqttTopic = "sensors/solaris"
+const parser = port.pipe(new Readline({ delimiter: '\r' }))
+
 process.env.TZ = 'Europe/Berlin';
 
-
-serial.on("open", function () {
-  console.log('open');
-  serial.on('data', function(line) {
+parser.on('data', function(line) {
     line = line.replace(/(\r\n|\n|\r)/gm,"");	
     var data = parseLine(line);
     writeFile(data);
     data = adjustFormat(data);
     mqttClient.publish(mqttTopic, JSON.stringify(data)); 
     console.log(data);	
-  });
 });
-
 
 mqttClient.on('connect', function () {
   mqttConnected = true;
@@ -57,9 +57,6 @@ mqttClient.on('error', function (e) {
   mqttConnected = false;
   console.log("mqtt->error", arguments);
 });
-
-
-
 
 function writeFile(data){
     var date = data.date;	
